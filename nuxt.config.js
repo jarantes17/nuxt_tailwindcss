1ecfd3ebae9784341de8aa2e29f26ca04d3cb52a
Jonatas Arantes <jarantes17@gmail.com>
2021-06-30 19:27:10 -0300

    Template colors has defined

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,5 +1,4 @@
 export default {
-  // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
     title: 'nuxt_tailwindcss',
     htmlAttrs: {
@@ -13,38 +12,35 @@ export default {
     link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }]
   },
 
-  // Global CSS: https://go.nuxtjs.dev/config-css
   css: [],
 
-  // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: ['@/plugins/vee-validate.ts'],
 
-  // Auto import components: https://go.nuxtjs.dev/config-components
   components: [{ path: '@/components', pathPrefix: false }],
 
-  // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
   buildModules: [
-    // https://go.nuxtjs.dev/typescript
     '@nuxt/typescript-build',
-    // https://go.nuxtjs.dev/tailwindcss
-    '@nuxtjs/tailwindcss'
+    '@nuxtjs/tailwindcss',
+    '@nuxtjs/google-fonts'
   ],
 
-  // Modules: https://go.nuxtjs.dev/config-modules
-  modules: [
-    // https://go.nuxtjs.dev/axios
-    '@nuxtjs/axios',
-    '@nuxtjs/style-resources'
-  ],
+  modules: ['@nuxtjs/axios', '@nuxtjs/style-resources'],
 
   styleResources: {
     scss: ['@/components/bosons/*.scss']
   },
 
-  // Axios module configuration: https://go.nuxtjs.dev/config-axios
   axios: {},
 
-  // Build Configuration: https://go.nuxtjs.dev/config-build
+  googleFonts: {
+    display: 'swap',
+    families: {
+      Inter: {
+        wght: [100, 200, 300, 400, 500, 600, 700, 800, 900]
+      }
+    }
+  },
+
   build: {
     transpile: ['vee-validate', 'vee-validate/dist/rules'],
     extractCSS: true
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,50 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+
 module.exports = {
   mode: 'jit',
   purge: [],
   darkMode: false,
   theme: {
     extend: {
+      colors: {
+        primary: {
+          base: '#d32f2f',
+          lightest: '#ff6659',
+          darkest: '#9a0007'
+        },
+        secondary: {
+          base: '#f57c00',
+          lightest: '#ffad42',
+          darkest: '#bb4d00'
+        },
+        success: {
+          base: '#4eca7a',
+          lightest: '#96cf59',
+          darkest: '#369a5d'
+        },
+        warning: {
+          base: '#ffa83f',
+          lightest: '#ffcc63',
+          darkest: '#ff895f'
+        },
+        danger: {
+          base: '#df484a',
+          lightest: '#f27075',
+          darkest: '#984848'
+        },
+        light: {
+          base: '#f7f7f7',
+          lightest: '#fcfcfc',
+          darkest: '#f0f0f0'
+        },
+        dark: {
+          base: '#7e7a7a',
+          lightest: '#a5a2a2',
+          darkest: '#292929'
+        }
+      },
       fontFamily: {
-        heading: ['Montserrat'],
-        sans: ['Oxygen'],
-        subheading: ['Muli']
+        sans: ['Inter', ...defaultTheme.fontFamily.sans]
       },
       animation: {
         shake: 'shake 1s both'
